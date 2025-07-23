93309ded1dcab9e6d24af43161e1ef705e17559e
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2025-07-23 11:54:34 +0530

    fix: routes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,38 +1,5 @@
-import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-const API_KEY = process.env.API_KEY || "";
-const PRICE_CONVERSION_URL = process.env.API_URL_PRICE_CONVERSION || "";
-
-// Conversion
-export async function convertPrice({
-  amount,
-  crypto,
-  fiat,
-}: {
-  amount: number;
-  crypto: string;
-  fiat: string;
-}) {
-  try {
-    console.log(amount, crypto, fiat);
-
-    const res = await axios.get(PRICE_CONVERSION_URL, {
-      headers: {
-        "X-CMC_PRO_API_KEY": API_KEY,
-      },
-      params: {
-        amount,
-        symbol: crypto,
-        convert: fiat,
-      },
-    });
-
-    return res.data;
-  } catch (err) {
-    console.error("Conversion error:", err);
-    return { error: "Conversion failed" };
-  }
-}
+import { convertPrice } from "@/components/convertor/utils";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
